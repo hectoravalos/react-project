6cdf18f1f47ffb739405f819f5b796185be24f5d
Hector Avalos <havalos711@gmail.com>
2019-03-19 16:22:39 -0700

    Updates

---
+++ precache-manifest.2078294c7ac70ab6ba76dfc94d6f3390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62f04524e7bdfe80d4166c2fb66f9562",
+    "url": "/react-project/static/media/Portland.62f04524.jpg"
+  },
+  {
+    "revision": "0e5970c7050e0b32f2c3",
+    "url": "/react-project/static/js/runtime~main.0e5970c7.js"
+  },
+  {
+    "revision": "a9c2dfc9498e5988a2f3",
+    "url": "/react-project/static/js/main.a9c2dfc9.chunk.js"
+  },
+  {
+    "revision": "d600a5225ef0e8f08a60",
+    "url": "/react-project/static/js/2.d600a522.chunk.js"
+  },
+  {
+    "revision": "a9c2dfc9498e5988a2f3",
+    "url": "/react-project/static/css/main.95f27f42.chunk.css"
+  },
+  {
+    "revision": "7cc446cb5294638b46bd0d22874b4a41",
+    "url": "/react-project/index.html"
+  }
+];
